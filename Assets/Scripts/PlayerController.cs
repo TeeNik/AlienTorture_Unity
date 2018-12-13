3910cdb7348bf783d3b9ea2b660302313509ec30
TeeNik <teenik3315@gmail.com>
2018-12-14 02:01:22 +0300

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float Speed = 2;
+
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * Speed;
+        float rotation = Input.GetAxis("Horizontal") * Speed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        Vector3 move = new Vector3(rotation, translation, 0);
+        //move.Normalize();
+
+        transform.Translate(move);
+    }
+}
