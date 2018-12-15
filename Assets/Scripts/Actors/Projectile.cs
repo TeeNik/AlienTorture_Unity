6168423daf0c6d487bd26c67935ecbbf358526bd
TeeNik <teenik3315@gmail.com>
2018-12-15 12:58:56 +0300

    base shooting

---
+++ Assets/Scripts/Actors/Projectile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public int Speed;
+
+
+
+
+}
---
+++ Assets/Scripts/System/BulletPool.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPool
+{
+
+    private List<GameObject> _pool;
+    private int _count;
+
+    void Init()
+    {
+        _pool = new List<GameObject>();
+    }
+
+
+    void GetBullet()
+    {
+
+    }
+
+
+
+
+
+}
