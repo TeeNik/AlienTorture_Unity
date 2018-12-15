43de6fee50c5aa6aa412574448961f724ad25fe9
TeeNik <teenik3315@gmail.com>
2018-12-15 22:15:49 +0300

    -

--- Assets/Scripts/Character/CharacterView.cs
+++ Assets/Scripts/Character/CharacterView.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
+using UnityEditor.Animations; 
 using UnityEngine;
 
 public class CharacterView : MonoBehaviour
 {
 
     [SerializeField] private SpriteRenderer _sr;
-    [SerializeField] private ANimation _sr;
 
 }
