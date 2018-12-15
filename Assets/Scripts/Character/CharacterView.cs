95b0577ca12d9a24e037ca098dad8db280c0c4f4
TeeNik <teenik3315@gmail.com>
2018-12-15 16:43:51 +0300

    character

---
+++ Assets/Scripts/Character/CharacterView.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class CharacterView : MonoBehaviour
+{
+
+    [SerializeField] private SpriteRenderer _sr;
+    [SerializeField] private ANimation _sr;
+
+}
