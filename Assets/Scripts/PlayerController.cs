f97efe659905f49e2477a699b8e4243f5fd2b4b4
TeeNik <teenik3315@gmail.com>
2018-12-15 22:50:17 +0300

    fix

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float Speed = 50f;
+    Rigidbody2D body;
+    Animator anim;
+    bool looksRight = true;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontal, vertical).normalized * Speed;
+        anim.SetFloat("Speed", body.velocity.magnitude);
+        if ((body.velocity.x > 0 && !looksRight) || (body.velocity.x < 0 && looksRight))
+            Flip();
+    }
+
+    void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        looksRight = !looksRight;
+    }
+
+}
---
+++ Assets/Scripts/WeaponDirection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDirection : MonoBehaviour
+{   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
+        float angle= 180 * Mathf.Atan2(mousePosition.y, mousePosition.x) / Mathf.PI; ;
+        int horizontalDirection= (mousePosition.x>0) ? 1 : -1; 
+        transform.rotation = Quaternion.Euler(0, 90-horizontalDirection*90,-90+ horizontalDirection*(90 + angle)); 
+    }
+}
