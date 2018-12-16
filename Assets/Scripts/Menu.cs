81344fa51a10011adccfe92141bccd51a13b44e5
Макс <max4nn@gmail.com>
2018-12-16 15:50:21 +0300

    main menu 0.2 + weapon fix

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    GameObject currentPage;
+    public GameObject[] pages;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentPage = pages[0];   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void OpenPage(int i)
+    {
+        currentPage.SetActive(false);
+        currentPage = pages[i];
+        currentPage.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
