0a72cbfa71402ca0d8c4a2eb6a0572df6525df0e
TeeNik <teenik3315@gmail.com>
2019-01-07 01:22:34 +0300

    -

--- Assets/Scripts/System/Reactive/CommandSubject.cs
+++ Assets/Scripts/System/Reactive/CommandSubject.cs
@@ -1,67 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Yarx.Subjects
+public delegate void Callback();
+public delegate void Callback<T>(T arg1);
+
+public class CommandSubject : IDisposable
 {
-    public delegate void Callback();
-    public delegate void Callback<T>(T arg1);
+    public Dictionary<Type, Delegate> _eventTable = new Dictionary<Type, Delegate>();
 
-    public class CommandSubject : IDisposable
+    public void Dispose()
     {
-        public Dictionary<Type, Delegate> _eventTable = new Dictionary<Type, Delegate>();
+        _eventTable.Clear();
+    }
 
-        public void Dispose()
+    public IDisposable Subscribe<T>(Callback<T> handler)
+    {
+        Type eventType = typeof(T);
+        if (!_eventTable.ContainsKey(eventType))
         {
-            _eventTable.Clear();
+            _eventTable.Add(eventType, null);
         }
-
-        public IDisposable Subscribe<T>(Callback<T> handler)
+        _eventTable[eventType] = (Callback<T>)_eventTable[eventType] + handler;
+        return new AnonymousDisposable(() =>
         {
-            Type eventType = typeof(T);
-            if (!_eventTable.ContainsKey(eventType))
-            {
-                _eventTable.Add(eventType, null);
-            }
-            _eventTable[eventType] = (Callback<T>)_eventTable[eventType] + handler;
-            return new AnonymousDisposable(() =>
-            {
-                RemoveListener<T>(handler);
-            });
-        }
+            RemoveListener<T>(handler);
+        });
+    }
 
-        private void RemoveListener<T>(Delegate handler)
+    private void RemoveListener<T>(Delegate handler)
+    {
+        Type eventType = typeof(T);
+        if (_eventTable.ContainsKey(eventType))
         {
-            Type eventType = typeof(T);
-            if (_eventTable.ContainsKey(eventType))
+            _eventTable[eventType] = Delegate.Remove(_eventTable[eventType], handler);
+            if (_eventTable[eventType] == null)
             {
-                _eventTable[eventType] = Delegate.Remove(_eventTable[eventType], handler);
-                if (_eventTable[eventType] == null)
-                {
-                    _eventTable.Remove(eventType);
-                }
+                _eventTable.Remove(eventType);
             }
         }
+    }
 
-        public void OnNext<T>()
+    public void OnNext<T>()
+    {
+        Delegate d;
+        Type eventType = typeof(T);
+
+        if (_eventTable.TryGetValue(eventType, out d))
         {
-            Delegate d;
-            Type eventType = typeof(T);
-
-            if (_eventTable.TryGetValue(eventType, out d))
-            {
-                (d as Callback)?.Invoke();
-            }
+            (d as Callback)?.Invoke();
         }
+    }
 
-        public void OnNext<T>(T arg)
+    public void OnNext<T>(T arg)
+    {
+        Delegate d;
+        Type eventType = typeof(T);
+
+        if (_eventTable.TryGetValue(eventType, out d))
         {
-            Delegate d;
-            Type eventType = typeof(T);
-
-            if (_eventTable.TryGetValue(eventType, out d))
-            {
-                (d as Callback<T>)?.Invoke(arg);
-            }
+            (d as Callback<T>)?.Invoke(arg);
         }
     }
 }
