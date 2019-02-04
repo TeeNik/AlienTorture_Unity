a5db950d0c4e641c59d6aedfe7a34e3666ec34aa
TeeNik <teenik3315@gmail.com>
2019-02-05 01:51:27 +0300

    quest obj [2]

--- Assets/Scripts/Actors/Interactive/QuestObject.cs
+++ Assets/Scripts/Actors/Interactive/QuestObject.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestObject : MonoBehaviour
+public class QuestObject : Interactive
 {
-    // Start is called before the first frame update
-    void Start()
+    private float _progress;
+    private bool _isNeedReload;
+
+    public override bool IsContinuous
     {
-        
+        get => true;
+        set { }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Process(Interactive obj)
     {
-        
+        if (_progress < 1)
+        {
+            _progress += .1f;
+            print(_progress);
+        }
+        else
+        {
+            if (!_isNeedReload)
+            {
+                _progress = 0;
+                var hands = obj as Hands;
+                if (hands.IsEmpty)
+                {
+                }
+                _isNeedReload = true;
+            }
+        }
+    }
+
+    public override void Leave(Interactive obj)
+    {
+        base.Leave(obj);
+        _isNeedReload = false;
     }
 }
