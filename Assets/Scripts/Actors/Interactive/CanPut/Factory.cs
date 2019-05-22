0a5ef67f395f37d93532711e16535f0e27ecab46
TeeNik <teenik3315@gmail.com>
2019-05-22 09:44:08 +0300

    craft

---
+++ Assets/Scripts/Actors/Interactive/CanPut/Factory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+public class FactoryData
+{
+    public string InputType;
+    public string OutputType;
+    public float CraftTime;
+}
+
+public class Factory : Interactive
+{
+    private float _progress;
+
+    public FactoryData Data { get; private set; }
+    public ProgressBar ProgressBar;
+
+    public override void Process(Interactive obj)
+    {
+        Hands hands = obj as Hands;
+        if (!hands.IsEmpty)
+        {
+            if (hands.InHands.Type == Data.InputType)
+            {
+
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    /*private IEnumerator CreateItem()
+    {
+
+    }*/ 
+
+    public void Put(Interactive obj)
+    {
+
+    }
+}
